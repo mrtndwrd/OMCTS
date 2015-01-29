6c02f5067ac99586f5188c98d4fa1012ac647054
Maarten de Waard <mrtndwrd@gmail.com>
2015-01-29 17:57:49 +0100

    improvement to testing scripst

--- scripts/getScore.sh
+++ scripts/getScore.sh
@@ -4,4 +4,4 @@ grep -v 'Result (1->win; 0->lose):-100, Score:-1000.0, timesteps:' | \
 	# Get all the rules with scores in them
 	grep 'Result (1->win; 0->lose)' | \
 	# Extract only the scores
-	sed -r 's/\s+\[java\] Result \(1->win; 0->lose\):([-]*[0-9]+), Score:([-]*[0-9]+.[0-9]+), timesteps:([-]*[0-9]+)/\1 \2 \3/g'
+	sed -r 's/\s*[][a-zA-Z]*\s*Result \(1->win; 0->lose\):([-]*[0-9]+), Score:([-]*[0-9]+.[0-9]+), timesteps:([-]*[0-9]+)/\1 \2 \3/g'
